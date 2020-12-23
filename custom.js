e990be5bee5d5581bc1efdf60c9c5e75b57acebd
denizmemduev <deniz.memduev@ue-varna.bg>
2020-12-23 20:43:10 +0200

    updated game

--- custom.js
+++ custom.js
@@ -2,67 +2,108 @@ let ben = document.getElementById("el1");
 let sagel1 = document.getElementById("el4");
 let sagel2 = document.getElementById("el5");
 let sagel3 = document.getElementById("el6");
-let butt = document.getElementsByTagName("button")[0];
+
 let kck1 = document.getElementById("kck1");
 let kck2 = document.getElementById("kck2");
 let kck3 = document.getElementById("kck3");
 let el = document.getElementById("elgo");
-var m = kck1(m);
-function kc1(m){
+
+let myscore = document.getElementById("par1");
+let compscore = document.getElementById("par2");
+
+var num;
+var b;
+var me=0;
+var comp=0;
+function kc1(){
     ben.setAttribute("src", `r1.png`);
-    m = "tas"
-    return m;
+   num=1;
+   
     
 }
-function kc2(m){
+function kc2(){
     ben.setAttribute("src", `p1.png`);
-    m="kagit";
-    return m;
+    num=2;
+    
 }
-function kc3(m){
+function kc3(){
     ben.setAttribute("src", `s1.png`);
-    m="makas";
-    return m;
+    num=3;
+   
 }
 
 
- function startt(n){
+ function startt(){
      
-  const  b = Math.round((Math.random()*3), 2);
+
+
+ b = Math.floor(Math.random() * (4 - 1) ) + 1;
     el.classList.add("gosterme");
-    console.log(b);
+  
     if(b==1){
-        console.log("asd");
+       
         sagel1.classList.add("goster");
         sagel2.classList.remove("goster");
         sagel3.classList.remove("goster");
-        n = "tas";
+        console.log(b);
+
+
+
+
+      
     }
     if(b==2){
-        console.log("asd");
+       
         sagel2.classList.add("goster");
         sagel1.classList.remove("goster");
         sagel3.classList.remove("goster");
-        n ="kagit";
+        console.log(b);
+       
     }
     if(b==3){
-        console.log("asd");
+      
         sagel3.classList.add("goster");
         sagel2.classList.remove("goster");
         sagel1.classList.remove("goster");
-        n ="makas";
+        console.log(b);
+  
     
     }
 }
-if(m==n){
-    console.log("hai");
-}
 
- //================================================================SKOR-=========================================================
- //===================================================================================
- //===================================================================
+function score() {
 
-function sonuc(){
-    console.log(m);
+    if(num==b){
+comp+=0;
+me+=0;
+    }
+    if(num==1 && b==2){
+     comp++;
+
+    }
+    if(num==2 && b==3){
+     comp++;
+   
+       }
+       if(num==3 && b==1){
+        comp++;
+      
+          }
+          if(num==2 && b==1){
+            me++;
+          
+              }
+              if(num==3 && b==2){
+                me++;
+              
+                  }
+                  if(num==1 && b==3){
+                    me++;
+                  
+                      }
+
+                      myscore.innerHTML=me;
+                      compscore.innerHTML=comp;
+console.log("Score: ",me,comp);
+
 }
-sonuc();
